c28a56c0651d3812d83db28fd3efb2a3147edceb
itzmitto <andre.babirian2004@gmail.com>
2026-06-01 13:52:31 +0200

    pagina design gemaakt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<body>
-	Hell Yeah!!
-</body> 
+<div class="flex flex-col h-dvh">
+  <header class="w-full h-16 bg-gray-200"></header>
+  <main class="flex-1 flex">
+    <aside class="w-1/5 bg-gray-300">
+      <div class="h-12 bg-gray-500 border-y text-center flex items-center px-2">Lorem Ipsum</div>
+      <div class="h-12 bg-gray-500 border-y text-center flex items-center px-2">Lorem Ipsum</div>
+      <div class="h-12 bg-gray-500 border-y text-center flex items-center px-2">Lorem Ipsum</div>
+      <div class="h-12 bg-gray-500 border-y text-center flex items-center px-2">Lorem Ipsum</div>
+      <div class="h-12 bg-gray-500 border-y text-center flex items-center px-2">Lorem Ipsum</div>
+    </aside>
+    <section class="flex-1 grid grid-rows-2 grid-cols-2 gap-8 p-8">
+      <div class="bg-gray-200 rounded-md w-full h-full"></div>
+      <div class="bg-gray-200 rounded-md w-full h-full"></div>
+      <div class="bg-gray-200 rounded-md w-full h-full"></div>
+      <div class="bg-gray-200 rounded-md w-full h-full"></div>
+    </section>
+  </main>
+</div>
