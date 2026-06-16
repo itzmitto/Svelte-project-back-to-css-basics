42942a572c8fa091e8f28c0a34df5c825096979d
Brian Post <bpostzakelijk@gmail.com>
2026-06-16 13:58:01 +0200

    service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+
+	export const prerender = true;
 
 	let { children } = $props();
+
+	onMount(async () => {
+		// Double check we are explicitly in a secure browser context
+		if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onOfflineReady() {
+					console.log('Success! Your CSS Editor works offline!');
+				}
+			});
+		}
+	});
 </script>
 
 <svelte:head>
